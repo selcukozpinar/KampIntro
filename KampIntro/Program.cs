8dfd8b6f8d4638a40dd9d75a9db66f9c9605e513
Selcuk <Selcuk@PC394>
2023-09-11 13:11:12 +0300

    Proje dosyası ekle.

---
+++ KampIntro/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KampIntro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety - tip güvenliği
+            //Do not repeat yourself - Kendini tekrarlma
+            string kategoriEtiketi = "Kategori";
+            int ogrencisayisi = 32000;
+            double faziOrani = 1.45;
+            bool sistemeGirisYapmisMi = false;
+            double dolarDun = 7.55;
+            double dolarBugun = 7.45;
+
+           if(dolarDun>dolarBugun)
+            {
+
+                Console.WriteLine("Azalış butonu");
+            }
+           else if (dolarDun<dolarBugun)
+
+            {
+
+                Console.WriteLine("artış Butonu");
+
+            }
+           else
+            {
+                Console.WriteLine("DEğişmdi");
+            }
+                
+                
+                
+                if (sistemeGirisYapmisMi == true)
+            {
+
+                Console.WriteLine("Kullanıcı ayarları butonu");
+
+
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+            
+
+        }
+    }
+}
